0b5bfe07df1e9d59ceb2d30d5d0ed5c4684513c2
Lukas Forst <lukas@forst.pw>
2020-03-07 21:12:42 +0100

    add publishing to bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `maven-publish`
     kotlin("jvm") version "1.3.70"
     id("net.nemerosa.versioning") version "2.8.2"
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "pw.forst"
@@ -36,9 +37,10 @@ val javadocJar by tasks.creating(Jar::class) {
     from(tasks.javadoc)
 }
 
+val publication = "exposed-upsert"
 publishing {
     publications {
-        register("gpr", MavenPublication::class) {
+        register("exposed-upsert", MavenPublication::class) {
             from(components["java"])
             artifact(sourcesJar)
             artifact(javadocJar)
@@ -55,4 +57,22 @@ publishing {
             }
         }
     }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_TOKEN")
+    publish = true
+    setPublications(publication)
+    pkg(delegateClosureOf<com.jfrog.bintray.gradle.BintrayExtension.PackageConfig> {
+        repo = "jvm-packages"
+        name = "exposed-upsert"
+        websiteUrl = "https://forst.pw"
+        githubRepo = "LukasForst/exposed-upsert"
+        vcsUrl = "https://github.com/LukasForst/exposed-upsert"
+        description = "Simple upsert implementation for Exposed and PostgreSQL"
+        setLabels("kotlin", "PostgreSQL", "Exposed", "Exposed-Extensions")
+        setLicenses("MIT")
+        desc = description
+    })
 }
